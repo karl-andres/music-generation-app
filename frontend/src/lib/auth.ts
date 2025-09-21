b46844a11a6f285873854f9b5077dc3d8363cb59
karl-andres <135479605+karl-andres@users.noreply.github.com>
2025-09-21 19:42:17 -0400

    initialize frontend and betterauth

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { db } from "~/server/db";
+
+const prisma = db
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+});
