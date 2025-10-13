fc42a6ea131e191b88a3a8908100f01752fea579
karl-andres <135479605+karl-andres@users.noreply.github.com>
2025-10-12 21:31:58 -0400

    delete create.tsx

--- frontend/src/components/create.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Button } from "~/components/ui/button"
-import { queueSong } from "~/actions/generation"
-
-export default function CreateSong() {
-    return <Button onClick={queueSong}>Generate Song</Button>
-}
